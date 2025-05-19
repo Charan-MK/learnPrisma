fc76680578ac2edbb30029889251838a22eade90
Charan-MK <121428200+Charan-MK@users.noreply.github.com>
2025-05-19 13:33:51 +0530

    adds new models with relations

--- index.ts
+++ index.ts
@@ -2,14 +2,38 @@ import { PrismaClient } from "./generated/prisma";
 const prisma = new PrismaClient();
 
 async function main() {
-    const user = await prisma.user.create({
+    // deletes all the users in the table User
+    await prisma.user.deleteMany();
+
+    // created a user with the specified details in data
+    const user1 = await prisma.user.create({
+        data: {
+            name: 'testName',
+            age: 35,
+            email: 'testName@email.com'
+        }
+    })
+    console.log('user1 created ', user1);
+    await prisma.user.deleteMany();
+
+    // since user prefernce is referenced by user, with prisma it is possible to create user prefernces with user model
+    const user2 = await prisma.user.create({
         data: {
             name: 'testName2',
-            email: 'testname2@email.com',
+            age: 32,
+            email: 'testName2@email.com',
+            preferences: {
+                create: {
+                    emailUpdates: true,
+                }
+            }
+        },
+        include: {      // will include the preferences created or associated with the user and will be returned
+            preferences: true,
         }
     })
 
-    console.log('user created: ', user);
+    console.log('user2 created: ', user2);
 }
 
 main()
